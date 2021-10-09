c2355270379c88103764c06c7056778a115d6835
Khushal Mehrotra <kmehrotra07@gmail.com>
2021-10-10 00:18:58 +0530

    using joblib

--- prediction.py
+++ prediction.py
@@ -8,6 +8,8 @@ import tensorflow as tf
 from tensorflow.keras import preprocessing
 from PIL import Image
 import pickle
+# from sklearn.externals import joblib
+import joblib
 
 def app():
     """This funciton runs the prediction page"""
@@ -17,17 +19,15 @@ def app():
     # Create a method to take text input from a user
     user_input = st.text_input("Enter the TEXT:")
     user_input_f = [user_input]
+    joblib_file1 = 'CV.pkl'
+    loaded_cv = joblib.load(joblib_file1)
+
+    joblib_file2 = 'Model_LR.pkl'
+    loaded_model = joblib.load(joblib_file2)
+
     # Create a button to get the prediction values on click
     if (st.button("Predict")):
         # load the model
-        cv_path = 'Count_Vect.pkl'
-        model_path = 'Model_LR.pkl'
-        with open(cv_path, 'rb') as file:  
-            loaded_cv = pickle.load(file)
-        
-        with open(model_path, 'rb') as file:  
-            loaded_model = pickle.load(file)
-     
         topred = loaded_cv.transform(user_input_f)
         pred = loaded_model.predict(topred)
         st.success("Prediction successful!!!")
